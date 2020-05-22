70e3f44e8a7bae5af92000f8cc37e956e8efe621
lalo <fx2013630461@gmail.com>
2020-05-22 17:58:36 -0500

    version entregada

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <ctype.h>  //Declares several functions that are useful for testing and mapping characters.
+#include <ctype.h>
 #include "tftp.h"
 #include "cmdline.h"
 
@@ -61,28 +61,30 @@ void data_send_cli ( tftp_t *instance ) {
 
     // Primero se espera el ACK del servidor y luego se responde un DATA
 
+    /* Esperamos el siguiente ack */
+
     received = recvfrom (
-                instance->local_descriptor, instance->buf, MAX_BUFSIZE, 0,
-                ( struct sockaddr * ) &instance->remote_addr, &instance->size_remote );
+        instance->local_descriptor, instance->buf, MAX_BUFSIZE, 0,
+        ( struct sockaddr * ) &instance->remote_addr, &instance->size_remote );
 
-    /*
-        Verificamos que haya llegado un msg válido, se debe cumplir:
+    /*  Verificamos que haya llegado un msg válido, se debe cumplir:
         1. Que received sea distinto a -1
-        2. Que el OPCODE sea OPCODE_ACK
-        3. Que el ack corresponda al blknum que esperamos
-        4. Que no haya expirado el socket
-    */
+        2. Que received sea distinto a EWOULDBLOCK
+        3. Que el OPCODE sea OPCODE_ACK
+        4. Que el ack corresponda al blknum que esperamos
+        5. Que el msg sea de donde lo esperamos (mismo tid del inicio de la
+        transferencia) */
 
-    if ( received != -1
+    if ( received != -1 && received != EWOULDBLOCK
          && ( ( instance->buf[0] << 8 ) + instance->buf[1] == OPCODE_ACK )
          && ( ( instance->buf[2] << 8 ) + instance->buf[3] == instance->blknum )
-         && received != EWOULDBLOCK
-         /*&& ( instance->tid == ntohs( instance->remote_addr.sin_port ) )*/ ) {
-        /*  Si hemos enviado el último msg y recibido el último ack, terminamos
-        */
+         && ( instance->tid == ntohs ( instance->remote_addr.sin_port ) ) ) {
+        instance->retries = 0;
+
+        /*  Si hemos enviado el último msg y recibido el último ack, terminamos  */
 
         if ( offset < BUFSIZE ) {
-            printf ( "File %s sent successful\n", instance->file );
+            syslog ( LOG_NOTICE, "File %s sent successfully", instance->file );
 
             /* Cerramos el descriptor de archivo y de socket */
 
@@ -91,10 +93,8 @@ void data_send_cli ( tftp_t *instance ) {
 
             /* Hijo finaliza */
 
-            _exit_free ( EXIT_SUCCESS, 1, instance );
+            _exit ( EXIT_SUCCESS );
         }
-
-        instance->retries = 0;
 
         /* Limpiamos los buffers */
 
@@ -112,48 +112,16 @@ void data_send_cli ( tftp_t *instance ) {
         if ( instance->blknum == 65536 )
             instance->blknum = 0;
 
-    } else {
-        timeout = true;
-        instance->retries++;
+        return;
 
-        if ( instance->retries == DEF_RETRIES ) {
-            puts ( "Retries limit reached." );
-            _exit ( EXIT_FAILURE );
-        }
+    }  // end 4-condition if
 
-        printf ( "Retrie number %d in data_send_client(); blknum %d\n",
-                 instance->retries, instance->blknum );
-    }
+    /*  Como no hemos recibido el ack correspondiente a la última trama que
+       hemos
+        enviado, ha expirado el tiempo de espera */
 
-    if ( timeout ) {
-        /* Regresamos 512 bytes por tener que reenviar el último msg */
-
-        lseek ( instance->fd, -BUFSIZE, SEEK_CUR );
-
-        /* Leemos 512 bytes del archivo */
-
-        offset = read ( instance->fd, instance->msg, BUFSIZE );
-
-        timeout = false;
-
-    } else
-        offset = read ( instance->fd, instance->msg, BUFSIZE );
-
-    /* Generamos el msg a enviar */
-
-    build_data_msg ( instance );
-
-    /* Enviamos el msg */
-
-    sent = sendto (
-                instance->local_descriptor, instance->buf, ACK_BUFSIZE + offset, 0,
-                ( struct sockaddr * ) &instance->remote_addr, instance->size_remote );
-
-    /* Verificamos que se haya enviado correctamente */
-
-    if ( sent != ACK_BUFSIZE + offset )
-        printf ( "Error from sendto() in data_send(): %s\n",
-                 strerror ( errno ) );
+    timeout = true;
+    instance->retries++;
 }
 
 void start_wrq ( tftp_t *instance ) {
@@ -161,11 +129,11 @@ void start_wrq ( tftp_t *instance ) {
 
     build_request ( instance, OPCODE_WRQ );
 
-    sent = sendto ( instance->local_descriptor, instance->buf, MAX_BUFSIZE, 0,
+    sent = sendto ( instance->local_descriptor, instance->buf, 4 + strlen(instance->file) + strlen(instance->mode), 0,
                     ( struct sockaddr * ) &instance->remote_addr,
                     instance->size_remote );
 
-    if ( sent != MAX_BUFSIZE ) {
+    if ( sent != 4 + strlen(instance->file) + strlen(instance->mode) ) {
         printf ( "ERROR Sending write request %s\n", strerror ( errno ) );
         close ( instance->local_descriptor );
         _exit_free ( EXIT_FAILURE, 1, instance );
@@ -208,33 +176,43 @@ void ack_send_cli ( tftp_t *instance ) {
     off_t   offset;
 
     /*  Asignamos -1 a blknum en caso de ser 65535 para evitar un rango
-        incorrecto en los ack */
+        incorrecto
+        en los ack */
 
     if ( instance->blknum == 65535 )
-        instance->blknum = 0;
+        instance->blknum = -1;
 
     /* Esperamos el siguiente msg */
 
     received = recvfrom (
-                instance->local_descriptor, instance->buf, MAX_BUFSIZE, 0,
-                ( struct sockaddr * ) &instance->remote_addr, &instance->size_remote );
+        instance->local_descriptor, instance->buf, MAX_BUFSIZE, 0,
+        ( struct sockaddr * ) &instance->remote_addr, &instance->size_remote );
 
     /* Verificamos que haya llegado un msg válido, se debe cumplir: */
     /* 1. Que received sea distinto a -1 */
-    /* 2. Que received sea distinto a  EWOULDBLOCK*/
+    /* 2. Que received sea distinto a EWOULDBLOCK */
     /* 3. Que el OPCODE sea OPCODE_DATA */
     /* 4. Que el blknum sea el que esperamos */
+    /*  5. Que el msg sea de donde lo esperamos (mismo tid del inicio de la
+        transferencia) */
 
     if ( received != -1 && received != EWOULDBLOCK
          && ( ( instance->buf[0] << 8 ) + instance->buf[1] == OPCODE_DATA )
          && ( ( instance->buf[2] << 8 ) + instance->buf[3]
-              == instance->blknum + 1 ) ) {
+              == instance->blknum + 1 )
+         && instance->tid == ntohs ( instance->remote_addr.sin_port ) ) {
+        /*  Llegando un msg válido, reiniciamos a cero el número máximo de
+            reintentos
+            permitidos */
+
         instance->retries = 0;
 
         /* Procesamos los datos recibidos */
+
         dec_data ( instance );
 
         /* Verificamos si es el último msg por recibir */
+
         if ( received < MAX_BUFSIZE ) {
             /* Escribimos en el archivo */
 
@@ -246,24 +224,26 @@ void ack_send_cli ( tftp_t *instance ) {
             build_ack_msg ( instance );
 
             /* Enviamos el último msg */
+
             sent = sendto ( instance->local_descriptor, instance->buf,
                             ACK_BUFSIZE, 0,
                             ( struct sockaddr * ) &instance->remote_addr,
                             instance->size_remote );
 
             /* Verificamos que se haya enviado correctamente */
-            if ( sent != ACK_BUFSIZE ) {
-                printf ( "Error from sendto() in ack_send(): %s\n",
-                         strerror ( errno ) );
-                _exit ( EXIT_FAILURE );
-            }
+
+            if ( sent != ACK_BUFSIZE )
+                _err_log_exit ( LOG_ERR,
+                                "Error from sendto() in ack_send(): %s",
+                                strerror ( errno ) );
 
             /* Cerramos el descriptor de archivo y de socket */
 
             close ( instance->fd );
             close ( instance->local_descriptor );
 
-            printf ( "Transfer successful: %s\n", instance->file );
+            syslog ( LOG_NOTICE, "File %s received successfully",
+                     instance->file );
 
             /* Hijo finaliza */
 
@@ -271,43 +251,28 @@ void ack_send_cli ( tftp_t *instance ) {
         }
 
         /* Escribimos en el archivo */
+
         offset = write ( instance->fd, instance->msg, received - ACK_BUFSIZE );
 
         /* Limpiamos buffer y msg */
+
         memset ( instance->buf, 0, MAX_BUFSIZE );
         memset ( instance->msg, 0, BUFSIZE );
 
         /* Incrementamos blknum */
+
         instance->blknum++;
+        return;
 
-    } else {  // end 4-condition if
+    }  // end 4-condition if
 
-        instance->retries++;
+    instance->retries++;
 
-        if ( instance->retries == DEF_RETRIES ) {
-            puts ( "Retries limit reached." );
-            _exit ( EXIT_FAILURE );
-        }
+    if ( instance->retries == DEF_RETRIES )
+        _err_log_exit ( LOG_ERR, "Retries limit reached." );
 
-        printf ( "Retrie number %d in ack_send(); blknum %d \n",
-                 instance->retries, instance->blknum );
-    }
-
-    /* Generamos el ack */
-    build_ack_msg ( instance );
-
-    /* Enviamos el ack */
-    sent = sendto ( instance->local_descriptor, instance->buf, ACK_BUFSIZE, 0,
-                    ( struct sockaddr * ) &instance->remote_addr,
-                    instance->size_remote );
-
-    /* Verificamos que se haya enviado correctamente */
-
-    if ( sent != ACK_BUFSIZE ) {
-        printf ( "Error from sendto() in ack_send(): %s\n",
-                 strerror ( errno ) );
-        _exit ( EXIT_FAILURE );
-    }
+    syslog ( LOG_NOTICE, "Retry number %d in ack_send(); blknum %d",
+             instance->retries, instance->blknum + 1 );
 }
 
 void start_rrq ( tftp_t *instance ) {
@@ -322,14 +287,9 @@ void start_rrq ( tftp_t *instance ) {
 
     build_request ( instance, OPCODE_RRQ );
 
-    // Enviamos el RRQ
 
-    sent = sendto ( instance->local_descriptor, instance->buf, MAX_BUFSIZE, 0,
-                    ( struct sockaddr * ) &instance->remote_addr,
-                    instance->size_remote );
-
-    if ( sent != MAX_BUFSIZE ) {
-        printf ( "ERROR Sending write request %s\n", strerror ( errno ) );
+    if ( sent != 4 + strlen(instance->file) + strlen(instance->mode) ) {
+        printf ( "ERROR Sending RRQ %s\n", strerror ( errno ) );
         _exit ( EXIT_FAILURE );
     }
 
@@ -351,59 +311,35 @@ void start_rrq ( tftp_t *instance ) {
     instance->fd     = open ( instance->file, O_WRONLY | O_CREAT | O_TRUNC,
                               S_IRWXU | S_IRWXG | S_IRWXO );
 
+    // Enviamos el RRQ
+
+    sent = sendto ( instance->local_descriptor, instance->buf, 4 + strlen(instance->file) + strlen(instance->mode), 0,
+                    ( struct sockaddr * ) &instance->remote_addr,
+                    instance->size_remote );
+
     /* Seguimos */
     for ( ;; )
         ack_send_cli ( instance );
 }
 
-void start_protocol ( char *ip, int type, char *file ) {
-    tftp_t *instance;
-    instance = malloc ( sizeof ( struct tftp ) );
+void start_protocol ( tftp_t *instance, int type ) {
 
-    // Inicializaciones generales
 
-    if ( !instance ) {
-        printf ( "ERROR Creating Client instance\n", strerror ( errno ) );
-        _exit ( EXIT_FAILURE );
-    }
-
-    instance->mode        = MODE_OCTET;
-    instance->size_remote = sizeof ( struct sockaddr_in );
-    instance->size_local  = sizeof ( struct sockaddr_in );
-    memset(instance->file, 0, 255);
-    strcpy(instance->file, file);
-
-    // Costruimos socket del servidor
-
-    memset ( &instance->remote_addr, 0, sizeof ( struct sockaddr_in ) );
-
-    if ( inet_pton ( AF_INET, ip, &instance->remote_addr ) != 1 ) {
-        printf ( "ERROR Parsing IPv4 server address %s\n", strerror ( errno ) );
-        _exit ( EXIT_FAILURE );
-    }
-
-    instance->remote_addr.sin_family = AF_INET;
-    instance->remote_addr.sin_port   = htons ( DEFAULT_SERVER_PORT );
 
     // Costruimos socket del cliente
 
     memset ( &instance->local_addr, 0, sizeof ( struct sockaddr_in ) );
     instance->local_descriptor = socket ( AF_INET, SOCK_DGRAM, 0 );
 
-    if ( instance->local_descriptor == -1 ) {
-        printf ( "ERROR Creating Client socket %s\n", strerror ( errno ) );
-        _exit ( EXIT_FAILURE );
-    }
-
-    instance->local_addr.sin_family      = AF_INET;
-    instance->local_addr.sin_addr.s_addr = INADDR_ANY;
-    // instance->local_addr.sin_port = htons(0);
-
     if ( bind ( instance->local_descriptor,
                 ( struct sockaddr * ) &instance->local_addr,
                 sizeof ( struct sockaddr_in ) )
          == -1 )
         printf ( "ERROR Binding Client socket %s\n", strerror ( errno ) );
+
+    instance->local_addr.sin_family      = AF_INET;
+    instance->local_addr.sin_addr.s_addr = INADDR_ANY;
+    instance->local_addr.sin_port = htons(0);
 
     /* Se ejecuta la peticion dependiendo del tipo que sea */
 
@@ -420,6 +356,21 @@ int main ( int argc, char **argv ) {
 
     struct gengetopt_args_info args_info;
     tftp_t instance;
+    int type;
+
+    instance.mode        = MODE_OCTET;
+
+    memset(&instance.remote_addr,0,sizeof(struct sockaddr_in));
+
+    instance.remote_addr.sin_family = AF_INET;
+    instance.size_remote = sizeof ( struct sockaddr_in );
+    instance.size_local  = sizeof ( struct sockaddr_in );
+    instance.remote_addr.sin_port = htons( DEFAULT_SERVER_PORT );
+
+
+
+
+
     /* Obtenemos las opciones de comando */
     if (cmdline_parser (argc, argv, &args_info) != 0)
         exit(EXIT_FAILURE) ;
@@ -437,11 +388,17 @@ int main ( int argc, char **argv ) {
         exit(EXIT_FAILURE);
     }
     printf("Número de argumentos sin nombre: %d\n", args_info.inputs_num);
-    if ( args_info.get_given )
+    if ( args_info.get_given ){
         printf( "get: %s\n", args_info.get_arg);
+        strcpy(instance.file,args_info.get_arg);
+        type = OPCODE_RRQ;
+    }
 
-    if ( args_info.put_given )
+    if ( args_info.put_given ) {
         printf( "put: %s\n", args_info.put_arg);
+        strcpy(instance.file, args_info.put_arg);
+        type = OPCODE_WRQ;
+    }
 
     /* Revisamos que sea una dirección y puerto válidos */
     /* Si no se especifica puerto, se usará el 69 */
@@ -451,39 +408,43 @@ int main ( int argc, char **argv ) {
         exit(EXIT_FAILURE);
     }
 
-    if (args_info.inputs_num <= 2) {  /* Solo nos interesan máximo dos parámetros, dirección y puerto. Si hay más, salimos */
-
-        for ( unsigned i = 0 ; i < args_info.inputs_num ; ++i ) { /* Deben ser en el orden "dirección puerto(opcional)" */
-
-            if ( i == 0) { /* Autenticamos la dirección del servidor */
-
-                if ( inet_pton ( AF_INET, args_info.inputs[i], &instance.remote_addr ) != 1 ) {
-                    printf ( "Error parsing IPv4 server address %s\n", strerror ( errno ) );
-                    _exit ( EXIT_FAILURE );
-                }
-                printf("%s es una IP válida\n",args_info.inputs[i]);
-                //IP válida
-            }
-
-            if ( i == 1) { /* Verificamos que el puerto esté en un rango válido ( 0 - 65535) */
-                char *tmp;
-                int number = strtol(args_info.inputs[i], &tmp, 10);
-
-                if (  ( errno == ERANGE )
-                      || ( number <= 0 )
-                      || ( number >= 65535 ) ) {
-                    printf ( "Error parsing port server  %s\n", strerror ( errno ) );
-                    _exit ( EXIT_FAILURE );
-                }
-                printf("%d es un puerto válido\n",number);
-                //Puerto válido
-            }
-        }
-    } else {
+    if (args_info.inputs_num > 2 ) {  /* Solo nos interesan máximo dos parámetros, dirección y puerto. Si hay más, salimos */
         puts( "Too much arguments." );
         exit(EXIT_FAILURE);
     }
+    for ( unsigned i = 0 ; i < args_info.inputs_num ; ++i ) { /* Deben ser en el orden "dirección puerto(opcional)" */
 
+        if ( i == 0) { /* Autenticamos la dirección del servidor */
+            if ( inet_pton ( AF_INET, args_info.inputs[i], &instance.remote_addr ) != 1 ) {// Copiamos la dirección en la estructura remote_addr
+                printf ( "Error parsing IPv4 server address %s\n", strerror ( errno ) );
+                _exit ( EXIT_FAILURE );
+            }
+            printf("%s es una IP válida\n",args_info.inputs[i]);
+
+
+        } else  { /* Verificamos que el puerto esté en un rango válido ( 0 - 65535) */
+
+            char *tmp;
+            int number = strtol(args_info.inputs[i], &tmp, 10);
+
+            if (  ( errno == ERANGE )
+                  || ( number <= 0 )
+                  || ( number >= 65535 ) ) {
+                printf ( "Error parsing port server  %s\n", strerror ( errno ) );
+                _exit ( EXIT_FAILURE );
+            }
+            printf("%d es un puerto válido\n",number);
+            instance.remote_addr.sin_port = htons(number);//copiamos el puerto
+        }
+    }
+   instance.remote_addr.sin_addr.s_addr = inet_addr("8.12.0.174");
+    instance.remote_addr.sin_family = AF_INET;
+    //instance.remote_addr.sin_port = htons( DEFAULT_SERVER_PORT );
+    instance.timeout.tv_usec =  DEF_TIMEOUT_USEC;
+    instance.timeout.tv_sec =  DEF_TIMEOUT_SEC;
     cmdline_parser_free (&args_info); /* liberamos la memoria alojada */
+    start_protocol(&instance, type);
+
+
     return EXIT_SUCCESS;
 }
